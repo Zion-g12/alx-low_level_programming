475a6abbc9ff268f0c52faef162a59dfff97f393
zion-g12 <tsiongirma525@gmial.com>
2023-08-16 17:08:41 -0700

    alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints the alphabet in lowercase,
+ *  * followed by a new line at the end.
+ *
+ */
+void print_alphabet(void)
+{
+char l;
+
+for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
+
+_putchar('\n');
+}
+
