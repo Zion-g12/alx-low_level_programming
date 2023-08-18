8e1507dc834aef4ff4ea6b5ca5cd30ca4073c6dd
zion-g12 <tsiongirma525@gmial.com>
2023-08-18 09:20:22 -0700

    prints number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - a function that print values
+ * Return: always return 0 value
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9 ; i++)
+{
+_putchar(i + '0');
+}
+
+_putchar('\n');
+}
