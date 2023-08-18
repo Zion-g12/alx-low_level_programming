f2bbc351a97323f7873afa739e8fe1a93b99b0d2
zion-g12 <tsiongirma525@gmial.com>
2023-08-18 05:13:18 -0700

    uppercase counting

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isuper- check the code.
+ *
+ * Return:  1 on success else 0.
+ */
+ int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+*main.h - Header file containing declarations for all functions
+*       used in the 0x04-functions_nested_loops directory.
+*/
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
