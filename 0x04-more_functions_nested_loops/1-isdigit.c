12a4dd121ac0b1c7a2b9e0512f876d6dbc05bd5a
zion-g12 <tsiongirma525@gmial.com>
2023-08-18 05:46:32 -0700

    checks whether a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isdigit - check the upper case letter
+ *@c: function parameters
+ * Return:  1 on success else 0.
+ */
+int _isdigit(int c)
+{
+
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+
+}
