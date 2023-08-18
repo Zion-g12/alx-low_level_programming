01d4e16756ea1734647bd9fbcd3fa515f9fbfb10
zion-g12 <tsiongirma525@gmial.com>
2023-08-18 05:31:37 -0700

    uppercase counting

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isuper- check the code.
- *
+ * _isupper - check the upper case letter
+ *@c: function parameters
  * Return:  1 on success else 0.
  */
- int _isupper(int c)
+int _isupper(int c)
 {
 
 if (c >= 'A' && c <= 'Z')
