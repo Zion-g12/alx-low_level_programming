999288b5081f17267f2c0c2fcd987197741b9f80
zion-g12 <tsiongirma525@gmial.com>
2023-08-18 10:03:11 -0700

    prints numbers that ommit 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - a function that print values
+ * print_most_numbers - a function that print values
  * Return: always return 0 value
  */
 void print_most_numbers(void)
 {
 int i;
-for (i = 0; i <= 9 ; i++)
+for (i = 0; i <= 9; i++)
 {
 if (i != 2 && i != 4)
 _putchar(i + '0');
 }
-
 _putchar('\n');
-} 
+}
