9b8d00c14b92fbce0c38c60b8b7763cac9551d5c
zion-g12 <tsiongirma525@gmial.com>
2023-08-21 01:44:25 -0700

    firs pointer commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - used to defrfernce a pointer value
+ *
+ * @n: that takes a pointer value
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+*main.h - Header file containing declarations for all functions
+*       used in the 0x04-functions_nested_loops directory.
+*/
+
+void reset_to_98(int *n);
+
+#endif
