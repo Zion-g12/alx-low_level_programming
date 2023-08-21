89e3f14423fab2ad4b072a5d8a370c9fcc084ac1
zion-g12 <tsiongirma525@gmial.com>
2023-08-21 02:46:04 -0700

    swap commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *swap_int  - a function that swaps two integers
+ *@a: the fist integer swapping value
+ *@b: takes the other swapping value
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
