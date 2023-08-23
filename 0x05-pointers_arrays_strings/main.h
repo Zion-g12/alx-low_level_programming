96fbcc4ece00b1c395f766df4c5ca6d54b82585f
zion-g12 <tsiongirma525@gmial.com>
2023-08-22 18:12:45 -0700

    printing a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 *main.h - Header file containing declarations for all functions
 *       used in the 0x04-functions_nested_loops directory.
 */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
