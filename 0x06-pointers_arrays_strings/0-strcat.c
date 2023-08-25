4f0cb860baadc6826f350ff63e9a2c2478a40cf9
zion-g12 <tsiongirma525@gmial.com>
2023-08-25 07:38:06 -0700

    string concatination

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,26 @@
 #include "main.h"
-#include <string.h>
 /**
- *_strcat - that append two strings
- *@src: the source stirng to be append
- *@dest: the destintion string the source to be added
- *Return: returns the value in ptr
- */
+*_strcat - concatinate 2 strings.
+*@dest: first string.
+*@src: second string.
+*Return: string.
+*/
+
 char *_strcat(char *dest, char *src)
 {
-size_t i, j;
-for (i = 0; dest[i] != '\0'; i++)
-for (j = 0; src[j] != '\0'; j++)
-dest[i + j] = src[j];
-dest[i + j] = '\0';
-return (dest);
-}
+	int i = 0, j = 0;
 
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-/**
-*main.h - Header file containing declarations for all functions
-*       used in the 0x04-functions_nested_loops directory.
-*/
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-#endif
