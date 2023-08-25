4663179f32d06610514f9e7667131336efe41dd8
zion-g12 <tsiongirma525@gmial.com>
2023-08-25 04:56:41 -0700

    appending 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-char ptr[50];
-strcpy(ptr, src);
-strcat(ptr, " ");
-strcat(ptr, dest);
-
-return ("ptr");
-
+size_t i, j;
+for (i = 0; dest[i] != '\0'; i++)
+for (j = 0; src[j] != '\0'; j++)
+dest[i + j] = src[j];
+dest[i + j] = '\0';
+return (dest);
 }
 
