b6ba4f1841e876bc30b3884f134fb4c6e5319a1c
tsion girma <tsiongirma525@gmail.com>
2023-09-06 20:06:40 -0700

    multiplication of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - argc argv mul
+ * @argc: arg cou
+ * @argv: vector
+ * Return: mul
+ */
 int main(int argc, char *argv[])
 {
-int num = 1 , i;
+int num;
+int i;
+num = 1;
+int num1;
 if (argc == 3)
 {
-for(i = 1; i < 3 ; i++)
+for (i = 1; i < 3; i++)
 {
-int num1 = strtol(argv[i], NULL , 10);
-num = num * num1;
+num1 = strtol(argv[i], NULL, 10);
+num *= num1;
 }
 printf("%d\n", num);
 }
 else
-{ 
+{
 printf("Error\n");
-return(1);
+return (1);
 }
-return(0);
+return (0);
 }
