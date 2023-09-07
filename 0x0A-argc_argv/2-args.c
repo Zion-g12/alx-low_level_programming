995af9286041032e4c1b8b7631ceb388ea5c0d6b
tsion girma <tsiongirma525@gmail.com>
2023-09-06 18:19:59 -0700

    argument printing commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc , char *argv[])
+{
+int i = 0;
+while(i < argc)
+{
+printf("%s\n" ,  argv[i]);
+ i++;
+}
+return (0);
+}
