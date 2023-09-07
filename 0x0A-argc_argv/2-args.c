4f934307c9287d4aec81804359fb6a862fbe513a
tsion girma <tsiongirma525@gmail.com>
2023-09-06 18:31:33 -0700

    a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ *main - a functoin that prints the argument it takes
+ *@argc: argument counter
+ *@argv[]: argument vector
+ *Return: always returns zero value
  */
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 int i = 0;
-while(i < argc)
+while (i < argc)
 {
-printf("%s\n" ,  argv[i]);
- i++;
+printf("%s\n",  argv[i]);
+i++;
 }
 return (0);
 }
