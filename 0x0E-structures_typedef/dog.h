0644486417bd1c3268007cd6652e382a0294a71a
tsion girma <tsiongirma525@gmail.com>
2023-09-11 18:22:07 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - a structure dog
+ *@name: a pointer to a char that store dog's name
+ *@age: a number that stores dog's age
+ *@owner: a pointer to char that stores dog's owner name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
